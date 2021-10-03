5b743d5dc9e293d8e51aebf1de5f970c2b8bb1dc
Coder7052 <80103867+Coder7052@users.noreply.github.com>
2021-10-03 13:25:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+ var x= document.getElementById("myAudio");
+ var input="";
+function play(){
+     document.getElementById("text_input").value = input
+    if (input = "pls play"){
+        x.play();
+    }
+}
